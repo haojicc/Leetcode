9d31191e74d4667ab58e065f43f5e904759fa88f
haojicc <87149183+haojicc@users.noreply.github.com>
2023-02-07 23:04:00 +0900

    Create 1137. N-th Tribonacci Number.cpp

---
+++ code/1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,34 @@
+/**
+The Tribonacci sequence Tn is defined as follows:
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+Given n, return the value of Tn.
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        int ans = 1;
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
+        for(int i = 3; i <= n; i++){
+            ans = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = ans;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+    int n = 25;
+    int res = obj.tribonacci(n);
+    cout << res << endl;
+}
