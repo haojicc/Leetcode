3cf134f774afc335b78d576b9657f08d74880573
haojicc <87149183+haojicc@users.noreply.github.com>
2023-02-07 23:02:00 +0900

    Create 746. Min Cost Climbing Stairs.cpp

---
+++ code/746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,31 @@
+/**
+You are given an integer array cost where cost[i] is the cost of ith step
+ on a staircase. Once you pay the cost, you can either climb one or two steps.
+You can either start from the step with index 0, or the step with index 1.
+Return the minimum cost to reach the top of the floor.
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int dp1 = 0;
+        int dp2 = 0;
+        for(int i = 2; i <= cost.size(); ++i){
+            int dp = min(dp1 + cost[i-1] ,dp2 + cost[i-2]);
+            dp2 = dp1;
+            dp1 = dp;
+        }
+        return dp1;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> cost{10,15,20};
+    int res = obj.minCostClimbingStairs(cost);
+    cout << res << endl;
+}
